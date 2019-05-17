9226fb5e74cd2696c8b159b4312be0c6045494c1
maximecroteau <maxime.croteau85@gmail.com>
2019-05-17 14:49:39 +0200

    add edit on ads

--- sixte/urls.py
+++ sixte/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('createad', views.create_ad, name='createad'),
     path('del_ad/<int:id>', views.del_ad, name='del_ad'),
+    path('edit_ad/<int:id>', views.edit_ad, name='edit_ad'),
 ]
--- sixte/views.py
+++ sixte/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import UpdateView
 
 from .forms import CreateAd
 
@@ -34,4 +35,21 @@ def del_ad(request, id):
     ads = Ad.objects.all()
     return render(request, 'menu/home.html', {
         'ads': ads
+        })
+
+
+def edit_ad(request, id):
+    ads_edits = Ad.objects.get(id=id)
+    form = CreateAd(request.POST, instance=ads_edits)
+    if form.is_valid():
+            form.save()
+            ads = Ad.objects.all()
+            return render(request, 'menu/home.html', {
+                'ads': ads
+                })
+    else:
+        form = CreateAd()
+    return render(request, 'menu/editad.html', {
+        'ads_edits': ads_edits,
+        'form': form,
         })
