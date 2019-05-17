98c5ba897dc7af5583732ae19b4b10ce114250df
maximecroteau <maxime.croteau85@gmail.com>
2019-05-17 15:30:52 +0200

    fix

--- sixte/urls.py
+++ sixte/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('createad', views.create_ad, name='createad'),
+    path('create_ad', views.create_ad, name='create_ad'),
     path('del_ad/<int:id>', views.del_ad, name='del_ad'),
     path('edit_ad/<int:id>', views.edit_ad, name='edit_ad'),
 ]
