2c3c30f86f68ac186b550eee927db504cb77a70b
maximecroteau <maxime.croteau85@gmail.com>
2019-05-31 11:22:25 +0200

    fix url redirect to /

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('sixte/', include('sixte.urls')),
+    path('', include('sixte.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
